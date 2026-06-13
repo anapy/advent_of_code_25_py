553842fc060326490842792dd07594d586574589
anapy <anabelenfunes@gmail.com>
2026-06-13 08:47:09 +0200

    add comments to explain code

--- day_6/ex_6.py
+++ day_6/ex_6.py
@@ -12,16 +12,18 @@ resultList = list()
 for i, line in enumerate(reversed(inputFile)):
     line = line.strip()
     items = line.split(' ')
-    #if first(last) line save operator in tuple
     pointer = 0
     for item in (items):
+        #if line is 0(in reverse) the operators, save it
         if i == 0:
             if item == '+' or item == '*':
                 resultList.append([item, '0'])
+        #if line is the first line with numbers, save them for later
         elif i == 1 and len(resultList) > 0:
             if item.isdigit():
                 resultList[pointer][1] = item
                 pointer += 1
+        #if line is from 2nd to last one, save the new result from add or multiply last result to the new number
         elif i > 1 and len(resultList) > 0:
             if item.isdigit():
                 resultList[pointer][1] = generateResult(item, resultList[pointer][1], resultList[pointer][0])
